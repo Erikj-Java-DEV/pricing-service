f51c556d01a27be8fd276e3a027e17c6db2d58a3
Erikj <erikpro606@gmail.com>
2026-09-24 14:52:57 +0200

    feat: validate price query parameters
    feat: validate price query parameters

--- src/main/java/com/erikj/pricing/infrastructure/adapter/in/rest/PriceController.java
+++ src/main/java/com/erikj/pricing/infrastructure/adapter/in/rest/PriceController.java
@@ -2,7 +2,9 @@ package com.erikj.pricing.infrastructure.adapter.in.rest;
 
 import com.erikj.pricing.application.port.in.GetApplicablePriceUseCase;
 import com.erikj.pricing.infrastructure.adapter.in.rest.dto.PriceResponse;
+import jakarta.validation.constraints.Positive;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,15 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/prices")
 public class PriceController {
 
     private final GetApplicablePriceUseCase getApplicablePriceUseCase;
 
-    public PriceController(
-            GetApplicablePriceUseCase getApplicablePriceUseCase
-    ) {
+    public PriceController(GetApplicablePriceUseCase getApplicablePriceUseCase) {
         this.getApplicablePriceUseCase = getApplicablePriceUseCase;
     }
 
@@ -29,9 +30,11 @@ public class PriceController {
             LocalDateTime applicationDate,
 
             @RequestParam
+            @Positive
             Long productId,
 
             @RequestParam
+            @Positive
             Long brandId
     ) {
         return PriceResponse.fromDomain(
